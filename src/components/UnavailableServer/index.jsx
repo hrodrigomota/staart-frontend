0fc3e9cac8aaa4c83913a17b3ff9001befcfd641
Hilton Rodrigo Mota <hrodrigomota@gmail.com>
2023-10-05 12:17:14 -0300

    add Unavailable Server component

--- src/components/UnavailableServer/index.jsx
+++ src/components/UnavailableServer/index.jsx
@@ -3,7 +3,7 @@ import cloudError from "/cloud-error.svg";
 export function UnavailableServer () {
   return (
     <div className="wrapper-server-error">
-      <img src={cloudError} alt="Página não encontrada" />
+      <img src={cloudError} alt="Erro no Servidor" />
       <h2>Serviço Indisponível </h2>
       <h3>Ocorreu um erro durante a requisição ao servidor. Tente novamente mais tarde!</h3>
     </div>
